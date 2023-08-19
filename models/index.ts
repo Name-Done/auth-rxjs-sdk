b50302a94ade834be5e163c16bba378b42615fa1
jonURT <123364219+jonURT@users.noreply.github.com>
2023-08-19 10:26:11 +0000

    Update SDK

--- models/index.ts
+++ models/index.ts
@@ -3,4 +3,5 @@ export * from './LoginRequestDto';
 export * from './LoginResponseDto';
 export * from './RefreshTokenRequestDto';
 export * from './RefreshTokenResponseDto';
+export * from './RegisterRequestDto';
 export * from './UnauthorizedResponseDto';
